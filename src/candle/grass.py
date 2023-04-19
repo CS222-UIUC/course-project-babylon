8225329b8634418dea71fe01c659d8bc5bb0c533
Chaoxiong <chaoxiong1311@outlook.com>
2023-04-19 12:11:47 -0500

    added documentation

--- src/candle/grass.py
+++ src/candle/grass.py
@@ -1,7 +1,19 @@
 import streamlit.components.v1 as components  # Import Streamlit
 import streamlit as st
+""" 
+--Liang created Apr 2 2023--
+last update Apr 19 2023
+""" 
 
+"""
+    Write an HTML file with the TradingView Widget for a given symbol.
 
+    Args:
+        symbol (str): The stock symbol to display in the TradingView Widget.
+
+    Returns:
+        None: The function writes the TradingView Widget HTML to 'currentstock.html'.
+"""
 def write_html_file(symbol):
   html = '''<!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
@@ -38,16 +50,28 @@ def write_html_file(symbol):
     </script>
   </div>
   <!-- TradingView Widget END -->'''
+
+  # Write the HTML code to the 'currentstock.html' file
   with open('currentstock.html', 'w') as f:
       f.write(html)
+"""
+    Display the stock graph for a given symbol using Streamlit and a TradingView Widget.
 
+    Args:
+        symbol (str, optional): The stock symbol to display. Defaults to 'RTX'.
+"""
 def display_graph(symbol='RTX'):
   file_name = 'currentstock.html'
+  # Write the TradingView Widget HTML for the given symbol
   write_html_file(symbol)
+  # Display a header in the Streamlit app
   st.header("test html import")
+  # Read the contents of the generated HTML file
   HtmlFile = open(file_name, 'r', encoding='utf-8')
-  source_code = HtmlFile.read() 
+  source_code = HtmlFile.read()
+  # Print the source code (for debugging purposes) 
   print(source_code)
+  # Display the TradingView Widget in the Streamlit app using the components module
   components.html(source_code,width=1000, height=500)
 
 display_graph()
